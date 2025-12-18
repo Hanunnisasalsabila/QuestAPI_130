94bd5e26cbd09028963d5336145544e21b69c5c3
Hanunnisasalsabila <hanunsalsabila83@gmail.com>
2025-12-18 17:51:04 +0700

    menambahkan endpoint GET untuk mengambil data siswa

--- app/src/main/java/com/example/praktikum10/apiservice/ServiceApiSiswa.kt
+++ app/src/main/java/com/example/praktikum10/apiservice/ServiceApiSiswa.kt
@@ -1,5 +1,10 @@
 package com.example.praktikum10.apiservice
 
+import com.example.praktikum10.modeldata.DataSiswa
+import retrofit2.http.GET
+
 interface ServiceApiSiswa {
+    @GET("bacaTeman.php")
+    suspend fun getSiswa(): List<DataSiswa>
 
 }
