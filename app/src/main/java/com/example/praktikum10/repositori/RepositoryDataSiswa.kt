6ca29d72cbe15f8c19ef6f10851544dce0bf38ba
Hanunnisasalsabila <hanunsalsabila83@gmail.com>
2025-12-18 18:10:24 +0700

    inisialisasi class implementasi repository dengan injeksi

--- app/src/main/java/com/example/praktikum10/repositori/RepositoryDataSiswa.kt
+++ app/src/main/java/com/example/praktikum10/repositori/RepositoryDataSiswa.kt
@@ -1,6 +1,8 @@
 package com.example.praktikum10.repositori
 
+import com.example.praktikum10.apiservice.ServiceApiSiswa
 import com.example.praktikum10.modeldata.DataSiswa
+import retrofit2.Response
 
 interface RepositoryDataSiswa {
     suspend fun getDataSiswa() : List<DataSiswa>
@@ -8,4 +10,18 @@ interface RepositoryDataSiswa {
     //suspend fun SatuSiswa(id: Int) : DataSiswa
     //suspend fun editSatuSiswa(id: Int, dataSiswa: DataSiswa):retrofit2.Response<Void>
     //suspend fun hapusSatuSiswa(id: Int):retrofit2.Response<Void>
+}
+
+class JaringanRepositoryDataSiswa(
+    private val serviceApiSiswa: ServiceApiSiswa
+): RepositoryDataSiswa{
+    override suspend fun getDataSiswa(): List<DataSiswa> = serviceApiSiswa
+        .getSiswa()
+
+    override suspend fun postDataSiswa(dataSiswa: DataSiswa):retrofit2
+    .Response<Void> = serviceApiSiswa.postSiswa(dataSiswa)
+
+    //override suspend fun getSatuSiswa(id: Int): DataSiswa = serviceApiSiswa.getSatuSiswa(id)
+    //override suspend fun editSatuSiswa(id: Int,dataSiswa: DataSiswa):retrofit2.Response<Void> = serviceApiSiswa.editSatuSiswa(id, dataSiswa)
+    //override suspend fun hapusSatuSiswa(id: Int):retrofit2.Response<Void> = serviceApiSiswa.hapusSatuSiswa(id)
 }
