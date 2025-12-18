704886e7a724bb4e84f918a2cbc779101dd742b9
Hanunnisasalsabila <hanunsalsabila83@gmail.com>
2025-12-18 19:16:01 +0700

    Setup navigasi utama aplikasi (DataSiswaApp & HostNavigasi)

---
+++ app/src/main/java/com/example/praktikum10/uicontroller/PetaNavigasi.kt
@@ -0,0 +1,39 @@
+package com.example.praktikum10.uicontroller
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.example.praktikum10.uicontroller.route.DestinasiEntry
+import com.example.praktikum10.uicontroller.route.DestinasiHome
+import com.example.praktikum10.view.EntrySiswaScreen
+import com.example.praktikum10.view.HomeScreen
+
+@Composable
+fun DataSiswaApp(navController: NavHostController = rememberNavController(),
+                 modifier: Modifier = Modifier){
+    HostNavigasi(navController = navController)
+}
+
+@Composable
+fun HostNavigasi(
+    navController: NavHostController,
+    modifier: Modifier = Modifier
+){
+    NavHost(navController = navController, startDestination = DestinasiHome.route,
+        modifier = Modifier ){
+        composable(DestinasiHome.route) {
+            HomeScreen(navigateToItemEntry = { navController.navigate(DestinasiEntry.route) },
+                navigateToItemUpdate = {
+//                    navController.navigate("${DestinasiDetail.route}/${it}")
+                })
+        }
+        composable(DestinasiEntry.route){
+            EntrySiswaScreen(navigateBack = { navController.navigate(DestinasiHome.route) })
+        }
+    }
+}
