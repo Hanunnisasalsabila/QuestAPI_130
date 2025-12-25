599b169aa78c3cab71e04740682d81912ba65ab2
Hanunnisasalsabila <hanunsalsabila83@gmail.com>
2025-12-26 04:15:12 +0700

    Menambahkan destinasi navigasi edit siswa

--- app/src/main/java/com/example/praktikum10/uicontroller/route/DestinasiEdit.kt
+++ app/src/main/java/com/example/praktikum10/uicontroller/route/DestinasiEdit.kt
@@ -1,2 +1,10 @@
 package com.example.praktikum10.uicontroller.route
 
+import com.example.praktikum10.R
+
+object DestinasiEdit: DestinasiNavigasi {
+    override val route = "item_edit"
+    override val titleRes = R.string.edit_siswa
+    const val itemIdArg = "idSiswa"
+    val routeWithArgs = "$route/{$itemIdArg}"
+}
