74cddcf0f7069acfb0eff4591aa2aad2c44df123
Hanunnisasalsabila <hanunsalsabila83@gmail.com>
2025-12-26 03:28:28 +0700

    Menambahkan fungsi untuk mengedit data siswa berdasarkan ID

--- app/src/main/java/com/example/praktikum10/repositori/RepositoryDataSiswa.kt
+++ app/src/main/java/com/example/praktikum10/repositori/RepositoryDataSiswa.kt
@@ -9,6 +9,8 @@ interface RepositoryDataSiswa {
     suspend fun postDataSiswa(dataSiswa: DataSiswa) : retrofit2.Response<Void>
 
     suspend fun getSatuSiswa(id:Int) : DataSiswa
+
+    suspend fun editSatuSiswa(id: Int, dataSiswa: DataSiswa): retrofit2.Response<Void>
 }
 
 class JaringanRepositoryDataSiswa(
@@ -21,4 +23,7 @@ class JaringanRepositoryDataSiswa(
         serviceApiSiswa.postSiswa(dataSiswa)
 
     override suspend fun getSatuSiswa(id: Int): DataSiswa = serviceApiSiswa.getSatuSiswa(id)
+
+    override suspend fun editSatuSiswa(id: Int, dataSiswa: DataSiswa): retrofit2.Response<Void> =
+        serviceApiSiswa.editSatuSiswa(id, dataSiswa)
 }
