ff9878cd87fcb00c99d04a9933ae0373d5650481
Hanunnisasalsabila <hanunsalsabila83@gmail.com>
2025-12-26 03:52:15 +0700

    menambahkan DetailViewModel untuk detail data siswa dan menambahkan StatusUIDetail untuk mengelola state UI detail siswa

--- app/src/main/java/com/example/praktikum10/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/praktikum10/viewmodel/DetailViewModel.kt
@@ -1,2 +1,22 @@
 package com.example.praktikum10.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import com.example.praktikum10.modeldata.DataSiswa
+import com.example.praktikum10.repositori.RepositoryDataSiswa
+import com.example.praktikum10.uicontroller.route.DestinasiDetail
+
+sealed interface StatusUIDetail {
+    data class Success(val satusiswa: DataSiswa) : StatusUIDetail
+    object Error : StatusUIDetail
+    object Loading : StatusUIDetail
+}
+class DetailViewModel (savedStateHandle: SavedStateHandle, private val repositoryDataSiswa:
+    RepositoryDataSiswa): ViewModel(){
+    private val idSiswa: Int = checkNotNull(savedStateHandle[DestinasiDetail.itemIdArg])
+    var statusUIDetail: StatusUIDetail by mutableStateOf(StatusUIDetail.Loading)
+        private set
+}
